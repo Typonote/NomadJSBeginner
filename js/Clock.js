bf68d8d834c996586f0dd15f4a8908d04c60fa6a
JinKyung Kim <kimradium@naver.com>
2021-12-16 14:50:54 +0900

    #3 시간 id와 변수 변경

--- js/Clock.js
+++ js/Clock.js
@@ -1,4 +1,4 @@
-const clock = document.querySelector("h2#clock");
+const clockTitle = document.querySelector("h2#clockTitle");
 
 const dayTitle = document.querySelector("h2#dayTitle");
 
@@ -9,17 +9,17 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function getDays() {
   const date = new Date();
-  const weekName = new Array("일", "월", "화", "수", "목", "금", "토");
+  const week = new Array("일", "월", "화", "수", "목", "금", "토");
   const month = String(date.getMonth() + 1);
   const day = String(date.getDate());
-  const yoil = String(weekName[date.getDay()]);
+  const weekTitle = String(week[date.getDay()]);
 
-  dayTitle.innerText = `${month}월 ${day}일 ${yoil}요일`;
+  dayTitle.innerText = `${month}월 ${day}일 ${weekTitle}요일`;
 }
 
 getDays();
